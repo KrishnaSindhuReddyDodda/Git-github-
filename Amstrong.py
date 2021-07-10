e0f17e334cff124fb83c0626fc251dbf1eb653cb
KrishnaSindhuReddyDodda <80235773+KrishnaSindhuReddyDodda@users.noreply.github.com>
2021-07-10 13:31:17 -0500

    Update Amstrong.py

--- Amstrong.py
+++ Amstrong.py
@@ -1,12 +1,13 @@
+#Single lined comments will start with '#' symbol
 import math                         # import all math files 
 num=int(input("Enter a value"))
 sum=0
 temp=num
 while(num!=0):
     r=num%10
-    sum=sum+pow(r,3)
+    sum=sum+pow(r,3)           
     num=num//10
-if(temp==sum):
+if(temp==sum):                      
     print("An Amstrong Number")
 else:
     print("Not an Amstrong Number")
